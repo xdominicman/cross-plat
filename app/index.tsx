9f89b8074e2aab517389cf4d3bf90e7b07d849f6
xdominicman <136241335+xdominicman@users.noreply.github.com>
2026-02-11 16:05:58 +0700

    tugas selesai

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, Text } from "react-native";
+import styles from "./styles.js";
 
-export default function Index() {
+export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.text}>Darryl Cornelius Setiawan - 00000098874</Text>
-    </View>
+      <Image
+        style={styles.image}
+        source={require("../assets/people/reynard.jpeg")}
+      />
+      <Text style={styles.text}>Reynard paling chill</Text>
+      <Image
+        style={styles.image}
+        source={require("../assets/people/aless.jpeg")}
+      />
+      <Text style={styles.text}>Aless the sigma</Text>
+      <Image
+        style={styles.image}
+        source={require("../assets/people/nivad.jpeg")}
+      />
+      <Text style={styles.text}>Nivad si pemakan</Text>
+      <Image
+        style={styles.image}
+        source={require("../assets/people/susilo dan pacar.jpeg")}
+      />
+      <Text style={styles.text}>Happy couple</Text>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#fff",
-  },
-});
---
+++ app/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    flexDirection: "column",
+  },
+  text: {
+    color: "#fff",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+export default styles;
